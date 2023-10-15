e69bcee01ebb677b5c7d1faf8dd552198259c7f0
ViktoriSharifullina <viktoriasharifullina@gmail.com>
2023-10-15 21:47:06 +0300

    worked on styles and forms

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -4,11 +4,11 @@ namespace App\Controller;
 
 use DateTime;
 use App\Entity\MicroPost;
+use App\Form\MicroPostType;
 use App\Repository\MicroPostRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MicroPostController extends AbstractController
@@ -32,12 +32,7 @@ class MicroPostController extends AbstractController
     #[Route('/micro-post/add', name: 'app_micro_post_add', priority: 2)]
     public function add(Request $request, MicroPostRepository $posts): Response
     {
-        $microPost = new MicroPost();
-        $form = $this->createFormBuilder($microPost)
-            ->add('title')
-            ->add('text')
-            ->add('submit', SubmitType::class, ['label' => 'Save'])
-            ->getForm();
+        $form = $this->createForm(MicroPostType::class, new MicroPost());
 
         $form->handleRequest($request);
 
@@ -55,6 +50,31 @@ class MicroPostController extends AbstractController
 
         return $this->renderForm(
             'micro_post/add.html.twig',
+            [
+                'form' => $form
+            ]
+        );
+    }
+
+    #[Route('/micro-post/{post}/edit', name: 'app_micro_post_edit')]
+    public function edit(MicroPost $post, Request $request, MicroPostRepository $posts): Response
+    {
+        $form = $this->createForm(MicroPostType::class, $post);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $post = $form->getData();
+            $posts->add($post, true);
+
+            // Add flush
+            $this->addFlash('success', 'Your micro post have been edited.');
+
+            return $this->redirectToRoute('app_micro_post_show', ['post' => $post->getId()]);
+        }
+
+        return $this->renderForm(
+            'micro_post/edit.html.twig',
             [
                 'form' => $form
             ]
